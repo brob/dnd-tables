4083f6489272970c8fbbfef6195018685ebf688f
Bryan Robinson <bryanlrobinson@gmail.com>
2024-01-19 14:30:20 -0500

    Initial commit

---
+++ src/data/treasures.js
@@ -0,0 +1,21 @@
+
+
+
+export const treasures = [
+    {
+        name: 'Treasure 1',
+        description: 'This is the first treasure',
+    },
+    {
+        name: 'Raw fish heads',
+        description: 'This is the second treasure',
+    },
+    {
+        name: 'Copper frog statue',
+        description: 'This is the third treasure',
+    },
+    {
+        name: 'Ugly, homemade doll',
+        description: 'This is the fourth treasure',
+    }
+];
